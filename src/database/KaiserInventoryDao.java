882bd7a85c933f2c9c5c56c1f69c72fa631551d7
KrazyKaiser12 <isaalghorazi@gmail.com>
2019-07-26 00:56:00 -0400

    [Resolved Issues with Dao]

--- src/database/KaiserInventoryDao.java
+++ src/database/KaiserInventoryDao.java
@@ -5,57 +5,65 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-public class KaiserInventoryDao 
-{
-	private Connection conn;
-	public KaiserInventoryDao() {
-		System.getenv("User");
-		try {
-			System.out.println("User");
-			conn = DriverManager.getConnection("jdbc:MySQL://localhost:3306/scannerproject","User","Pass");
-		} catch (SQLException e) {
+public class KaiserInventoryDao {
+private Connection conn;
+
+public KaiserInventoryDao() {
 		
+	
+	try {
+			System.out.println("User");
+			
+				conn = DriverManager.getConnection("jdbc:MySQL://localhost:3306/scannerproject", "**", "**");
+			
+					} catch (SQLException e) {
+
 			e.printStackTrace();
 		}
-				
+
 	}
-	
-	
+
+	public KaiserInventoryDto retrieveSingleStoreItem(String barcode) throws SQLException {
+		String ser = "select * from inventory WHERE barcode = " + barcode;
 		
-	public  KaiserInventoryDto retrieveSingleStoreItem(String barcode) throws SQLException 
-	{
-		 String ser = ("select * from inventory WHERE barcode = " + barcode);
-		 Statement now = conn.createStatement();
-		 ResultSet res = now.executeQuery(ser);
-		 
-		 return new KaiserInventoryDto( res.getFloat("prices"), barcode, res.getString("name") );
-				 
+		Statement now = conn.createStatement();
+		
+			ResultSet res = now.executeQuery(ser);
+
+		return new KaiserInventoryDto(res.getFloat("prices"), barcode, res.getString("name"));
+		
 	}
+
+	public void insertSingleStoreItem(KaiserInventoryDto kaiserInventoryDto, String barcode, Float prices)throws SQLException {
 	
-	public void insertSingleStoreItem(KaiserInventoryDto kaiserInventoryDto ,String barcode,Float prices) throws SQLException
-	{
-	
+
 		Statement itd = conn.createStatement();
 		try {
-			int insert = itd.executeUpdate("INSERT into inventory(id,barcode,prices )WHERE barcode = " + barcode);
-			System.out.println(insert +"Row Inserted");
-		} catch (SQLException e) {
-		
+				int insert = itd.executeUpdate("INSERT into inventory(id,barcode,prices )WHERE barcode = " + barcode);
+			
+					
+			
+				} catch (SQLException e) {
+
 			e.printStackTrace();
 		}
-	
+
 	}
-	
-	public KaiserInventoryDto updateSingleStoreItem(String barcode, float price) throws SQLException
-	{
-		Statement updat=conn.createStatement();
-	String updatequery = "update inventory "
-			+"set prices = 'prices ' "
-			+ "WHERE barcode= " + barcode;
-	updat.executeUpdate("updateQuery");
-	System.out.println("Update complete");
+
+	public KaiserInventoryDto updateSingleStoreItem(String barcode, float price) throws SQLException {
 		
+		Statement updat = conn.createStatement();
+		
+			String updateQeuery = "update inventory " 
+			+ 	"set prices = 'prices ' "
+			+		"" + "WHERE barcode= " + barcode;
+		
+				updat.executeUpdate("updateQuery");
+		
+			System.out.println("Update complete");
+		
+
 		return null;
 	}
-	
+
 }
